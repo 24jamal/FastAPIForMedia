e4b96f8dfea88a083446f1494eaa2a9528efc50e
jamaludeen750@gmail.com <jamal@MyPC.>
2026-01-16 18:36:48 +0000

    Database Connection

--- app/app.py
+++ app/app.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from app.schemas import PostCreate, PostResponse
-app = FastAPI()
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan = lifespan)
 
 text_posts = {
     1 : {"Title" : "Atomic habits", "Content": "Self help book with awesome habit tricks"},
